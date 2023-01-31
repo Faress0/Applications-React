a398546152b712b31e738d64dc46f5062e936fe2
Farès Boulebtina <Fares512004@gmail.com>
2023-01-31 09:16:18 +0100

    Application react qui tri les pays par nombre et continent.

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum recusandae odio ipsum molestiae quidem officiis beatae. Cupiditate, magni sint sequi, inventore laboriosam debitis hic sit necessitatibus nobis mollitia, minus reprehenderit? Ex modi delectus asperiores doloremque consequatur accusantium perspiciatis, ea possimus quae nulla. Esse nulla inventore, mollitia sint tempora cum, aut perferendis accusantium facere autem itaque aliquid. Dolore voluptate, autem a distinctio inventore alias accusantium voluptates corrupti optio illo dolores quae, quam molestias exercitationem facere incidunt et officiis qui architecto dignissimos ullam modi ut eligendi at. Autem atque neque molestias amet expedita dolor aliquam maiores deserunt ad, obcaecati quasi vitae commodi.</p>
+            <br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis autem nesciunt, perferendis accusamus, voluptatum hic atque fuga laboriosam eius quas sapiente aspernatur repellat. Ea nemo provident nisi magnam pariatur molestiae adipisci. Unde, veniam alias. Repellat voluptatum facilis quae qui laborum in! Ex repudiandae debitis expedita voluptates dolorum nesciunt facilis, nisi omnis ratione dolor dolore! Temporibus quos expedita perspiciatis labore veniam qui, repellendus quasi facilis suscipit, laudantium, soluta quo et ipsa repellat vitae quidem fugit esse distinctio. Molestias beatae doloremque dolores. Id, beatae rerum! Quia temporibus in asperiores quos voluptatibus alias. Debitis in quos, quae, autem corporis exercitationem, architecto inventore pariatur perspiciatis non illo voluptas totam. Natus pariatur esse vero odit dicta, explicabo ex exercitationem vitae, nostrum perspiciatis saepe similique laboriosam obcaecati facere! Animi pariatur sequi voluptatibus minus libero iusto corrupti laborum expedita nisi facilis est reprehenderit laudantium minima nesciunt placeat, quaerat perferendis debitis aut et dolore! Fugit ex, qui possimus saepe repellendus ipsam dicta nobis consequuntur alias. Quisquam cupiditate excepturi impedit quod voluptatem accusantium necessitatibus nihil blanditiis dolore, quis consequuntur. Asperiores fugiat, quia fuga pariatur architecto ad beatae nesciunt laboriosam expedita placeat officiis distinctio repellat nostrum vero eaque neque impedit laudantium dolorum, molestiae aliquam eum quis necessitatibus unde omnis? Nam.</p>
+        </div>
+    );
+};
+
+export default About;
